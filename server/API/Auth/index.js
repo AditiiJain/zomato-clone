6744aecbf38aed7190a91bfe81d820ed3555247e
AditiiJain <iaditi.jain08@gmail.com>
2021-10-10 16:12:07 +0530

    signup api

--- server/API/Auth/index.js
+++ server/API/Auth/index.js
@@ -16,27 +16,31 @@ Method          POST
 */
 Router.post("/signup", async (req, res) => {
   try {
-    const { email, password, fullname, phoneNumber } = req.body.credentials;
+    // const { email, password, fullname, phoneNumber } = req.body.credentials;
     //check whether email or phone number exists
-    const checkUserByEmail = await UserModel.findOne({ email });
-    const checkUserByPhone = await UserModel.findOne({ phoneNumber });
-    if (checkUserByEmail || checkUserByPhone) {
-      return res.json({ error: "User Already Exists!" });
-    }
+    // const checkUserByEmail = await UserModel.findOne({ email });
+    // const checkUserByPhone = await UserModel.findOne({ phoneNumber });
+    // if (checkUserByEmail || checkUserByPhone) {
+    //   return res.json({ error: "User Already Exists!" });
+    // }
+
+    await UserModel.findEmailAndPhone(req.body.credentials);
 
     //hashing and salting
-    const bcryptSalt = await bcrypt.genSalt(8);
-    const hashedPassword = await bcrypt.hash(password, bcryptSalt);
+    // const bcryptSalt = await bcrypt.genSalt(8);
+    // const hashedPassword = await bcrypt.hash(password, bcryptSalt);
 
     //DB -> storing each field
     //.create()->creates another field in DB
-    await UserModel.create({
-      ...req.body.credentials,
-      password: hashedPassword,
-    });
+    // await UserModel.create({
+    //   ...req.body.credentials,
+    // password: hashedPassword,
+    // });
+    const newUser=await UserModel.create(req.body.credentials);
 
     //JWT Auth Token
-    const token = jwt.sign({ user: { fullname, email } }, "ZomatoApp");
+    // const token = jwt.sign({ user: { fullname, email } }, "ZomatoApp");
+    const token = newUser.generateJwtToken();
 
     return res.status(200).json({ token });
   } catch (error) {
@@ -49,3 +53,7 @@ export default Router;
 //hashing vs salting
 //encrypted into some non understandable code is called hashing
 //hashing over and over again is called salting
+
+//statcis and methods
+//methods-> first create instance and then you use
+//statics-> you can directly use them without creating an instance
--- server/database/user/index.js
+++ server/database/user/index.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const UserSchema = new mongoose.Schema(
   {
     fullname: { type: String, required: true },
     email: { type: String, required: true },
-    password: { type: String,required:true },
+    password: { type: String, required: true },
     address: [{ details: { type: String }, for: { type: String } }],
     phoneNumber: [{ type: Number }],
   },
@@ -12,5 +14,46 @@ const UserSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+UserSchema.methods.generateJwtToken = function () {
+  return jwt.sign({ user: this._id.toString() }, "ZomatoApp");
+};
+
+UserSchema.statics.findEmailAndPhone = async ({ email, phoneNumber }) => {
+  //check whether the email exists
+  const checkUserByEmail = await UserModel.findOne({ email });
+  //check whether the phoneNumber exists
+  const checkUserByPhone = await UserModel.findOne({ phoneNumber });
+  if (checkUserByEmail || checkUserByPhone)
+    {
+    //we can't return statement when we are using statics
+    throw new Error("user already exists!");
+  }
+  return false;
+};
+
+//first parameter-> tells what and why action you are going to take
+//second parameter-> what will happen after the action
+//pre-> A lot of things are happening simultaneously, so inside the pre function, we want the function to be executed whenever we are creating a new DB and save it.{whenever we are saving something, the pre function should be executed}
+UserSchema.pre("save", function (next) {
+  const user = this; //here this refers to current user\
+
+  //password is not modified
+  if (!user.isModified("password")) return next();
+
+  //generating bcrypt salt
+  bcrypt.genSalt(8, (error, salt) => {
+    if (error) return next(error);
+
+    //hashing this password
+    bcrypt.hash(user.password, salt, (error, hash) => {
+      if (error) return next(error);
+
+      //assigning hashed password
+      user.password = hash;
+      return next();
+    });
+  });
+});
 
 export const UserModel = mongoose.model("users", UserSchema);
