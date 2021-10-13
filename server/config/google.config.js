0843c0c20e33f1b45e66f59a19eabde9354d4aad
AditiiJain <iaditi.jain08@gmail.com>
2021-10-13 23:58:47 +0530

    adding google authentication

--- server/config/google.config.js
+++ server/config/google.config.js
@@ -12,20 +12,28 @@ export default (passport) => {
         callbackURL: "http://localhost:4000/auth/google/callback",
       },
       async (accessToken, refreshToken, profile, done) => {
+        //creating new user
         const newUser = {
           fullname: profile.displayName,
           email: profile.emails[0].value,
           profilePic: profile.photos[0].value,
         };
         try {
+          //check whether user exists or not?
           const user = await UserModel.findOne({ email: newUser.email });
-          const token = user.generateJwtToken();
+
           if (user) {
+            // generating JWT token
+            const token = user.generateJwtToken();
             //first parameter->describes error
             //second parameter->information
+            //return user
             done(null, { user, token });
           } else {
+            //creating a new user
             user = await UserModel.create(newUser);
+            //return user
+            done(null, { user, token });
           }
         } catch (error) {
           done(error, null);
@@ -33,4 +41,6 @@ export default (passport) => {
       }
     )
   );
+  passport.serializeUser((userData, done) => done(null, { ...userData }));
+  passport.deserializeUser((id, done) => done(nll, id));
 };
